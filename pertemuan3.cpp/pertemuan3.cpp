73b2cace9683b2970a758eba8bae987bb08cbfb2
Mzahdul <seeglas8@gmail.com>
2026-03-02 20:17:32 +0700

    ActFunction_0234

---
+++ pertemuan3.cpp/pertemuan3.cpp
@@ -0,0 +1,12 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variable global
+int panjang, lebar;
+//implementasi fungsi dan  prosedur
+void input(){
+    cout << "Masukkan panjang : ";
+    cin >> panjang;
+    cout << "Masukkan lebar : ";
+    cin >> lebar;
+}
