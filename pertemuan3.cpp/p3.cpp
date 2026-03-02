5e18c13771a8bdbbb252e967086866087c2ed0e4
Mzahdul <seeglas8@gmail.com>
2026-03-02 21:27:14 +0700

    ActFunction_0234_2

--- pertemuan3.cpp/p3.cpp
+++ pertemuan3.cpp/p3.cpp
@@ -4,3 +4,12 @@ using namespace std;
 //deklarasi variable
 int p, l;
 //implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan panjang : ";
+    cin >> p;
+    cout << "Masukkan lebar : ";
+    cin >> l;
+}
+int luaspersegi(int a,int b){
+    return p * l;
+}
