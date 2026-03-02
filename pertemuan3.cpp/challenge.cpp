884ca88f2e48f242795983c12c5e6968a6aea048
Mzahdul <seeglas8@gmail.com>
2026-03-02 21:43:52 +0700

    ActFunction_0234_2

--- pertemuan3.cpp/challenge.cpp
+++ pertemuan3.cpp/challenge.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 float luas, d1, d2;
+
+void prosedurinputData(){
+    cout << "Masukkan diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2 : ";
+    cin >> d2;
+}
+
+float HitungLuasBerapameter(float d1, float d2){
+}
